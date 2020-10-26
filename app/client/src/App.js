72c137ab124a9c61c1d0dbb67cc96252e89559d1
RishabAn <49519517+RishabAn@users.noreply.github.com>
2020-10-27 01:50:52 +0530

    Final API call implemented to get Reviews linked to top 10 ice cream business

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,33 +1,71 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   state = {
-    response: '',
-    post: '',
-    responseToPost: '',
+    response: [
+      {
+        name: "",
+        address: "",
+        review: "",
+        username: "",
+      },
+    ],
   };
-  
+
   componentDidMount() {
     this.callApi()
-      .then(res => this.setState({ response: res}))
-      .catch(err => console.log(err));
+      .then((res) => this.setState({ response: res }))
+      .catch((err) => console.log(err));
   }
-  
+
   callApi = async () => {
-    const response = await fetch('/api/redwood/icecream');
+    const response = await fetch("/api/redwood/icecream");
     const body = await response.json();
     if (response.status !== 200) throw Error(body.message);
-    
-    return body;
+    var array = [];
+    body.forEach((element) => {
+      var obj = {};
+      obj.name = element.name;
+      obj.address = element.location.address1 + element.location.city;
+      obj.review = element.review;
+      obj.username = element.username;
+      array.push(obj);
+    });
+    return array;
   };
-  
-render() {
+
+  render() {
     return (
-      <div className="App">
-        <p>Top ten Ice cream shops in Redwood City</p>
-        <p>{this.state.response.text}</p>
+      <div>
+        <div className="App">
+          <p>Top ten Ice cream shops in Redwood City</p>
+        </div>
+        <div>
+          {this.state.response.map((item) => {
+            return (
+              <div className="yellow">
+                <div className="flex">
+                  <p className="blue">Business Name: </p>
+                  <p className="purple">{item.name}</p>
+                </div>
+                <div className="flex">
+                  <p className="blue">Address: </p>
+                  <p className="purple">{item.address}</p>
+                </div>
+                <div className="flex">
+                  <p className="blue">Top Review: </p>
+                  <p className="purple">{item.review}</p>
+                </div>
+                <div className="flex">
+                  <p className="blue">Review By: </p>
+                  <p className="purple">{item.username}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
--- app/server.js
+++ app/server.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const yelp = require("yelp-fusion");
+const Step = require("step");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,8 +9,109 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/redwood/icecream', (req, res) => {
-  res.send({ text: 'Ice cream shops in Redwood City' });
+app.get("/api/redwood/icecream", (req, resp) => {
+  const apiKey = process.env.YELP_API_KEY;
+
+  // Search Parameters to Loads Top 10 ice cream shop in redwood city
+  const searchRequest = {
+    location: "Redwood City",
+    term: "Ice Cream",
+    limit: 10,
+    sort_by: "rating",
+  };
+
+  const client = yelp.client(apiKey);
+  var data = [];
+  Step(
+    // Loads Top 10 ice cream shop in redwood city
+    function loadIcecreamData() {
+      client.search(searchRequest).then((res) => {
+        data = res.jsonBody.businesses;
+        this(null, res);
+      });
+    },
+    // Loads Reviews in series as in parallel yelp end point giving 'end-point-per-second' hit limit reached error
+    function loadReviewOne(err, response) {
+      if (err) {
+        console.log(err);
+      }
+      client.reviews(data[0].id).then((res) => {
+        data[0]["review"] = JSON.parse(res.body).reviews[0].text;
+        data[0]["username"] = JSON.parse(res.body).reviews[0].user.name;
+        this(null, res);
+      });
+    },
+    function loadReviewTwo(err, response) {
+      client.reviews(data[1].id).then((res) => {
+        data[1]["review"] = JSON.parse(res.body).reviews[0].text;
+        data[1]["username"] = JSON.parse(res.body).reviews[0].user.name;
+        this(null, res);
+      });
+    },
+    function loadReviewThree(err, response) {
+      client.reviews(data[2].id).then((res) => {
+        data[2]["review"] = JSON.parse(res.body).reviews[0].text;
+        data[2]["username"] = JSON.parse(res.body).reviews[0].user.name;
+        this(null, res);
+      });
+    },
+    function loadReviewFour(err, response) {
+      client.reviews(data[3].id).then((res) => {
+        data[3]["review"] = JSON.parse(res.body).reviews[0].text;
+        data[3]["username"] = JSON.parse(res.body).reviews[0].user.name;
+        this(null, res);
+      });
+    },
+    function loadReviewFive(err, response) {
+      client.reviews(data[4].id).then((res) => {
+        data[4]["review"] = JSON.parse(res.body).reviews[0].text;
+        data[4]["username"] = JSON.parse(res.body).reviews[0].user.name;
+        this(null, res);
+      });
+    },
+    function loadReviewSix(err, response) {
+      client.reviews(data[5].id).then((res) => {
+        data[5]["review"] = JSON.parse(res.body).reviews[0].text;
+        data[5]["username"] = JSON.parse(res.body).reviews[0].user.name;
+        this(null, res);
+      });
+    },
+    function loadReviewSeven(err, response) {
+      client.reviews(data[6].id).then((res) => {
+        data[6]["review"] = JSON.parse(res.body).reviews[0].text;
+        data[6]["username"] = JSON.parse(res.body).reviews[0].user.name;
+        this(null, res);
+      });
+    },
+    function loadReviewEight(err, response) {
+      client.reviews(data[7].id).then((res) => {
+        data[7]["review"] = JSON.parse(res.body).reviews[0].text;
+        data[7]["username"] = JSON.parse(res.body).reviews[0].user.name;
+        this(null, res);
+      });
+    },
+    function loadReviewNine(err, response) {
+      client.reviews(data[8].id).then((res) => {
+        data[8]["review"] = JSON.parse(res.body).reviews[0].text;
+        data[8]["username"] = JSON.parse(res.body).reviews[0].user.name;
+        this(null, res);
+      });
+    },
+    function loadReviewTen(err, response) {
+      client.reviews(data[9].id).then((res) => {
+        data[9]["review"] = JSON.parse(res.body).reviews[0].text;
+        data[9]["username"] = JSON.parse(res.body).reviews[0].user.name;
+        this(null, res);
+      });
+    },
+    // Send the final response
+    function showStuff(err, response) {
+      if (err) {
+        console.log(err);
+      }
+      resp.send(JSON.stringify(data));
+    }
+  );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
